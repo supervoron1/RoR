070e6489aa4d111ae9f897b9120b3b94aa3a74f1
Dmitriy Vorontsov <vorontsof@mail.ru>
2019-03-31 00:09:19 +0400

    reload

---
+++ lesson1/equation.rb
@@ -0,0 +1,20 @@
+puts "Введите коофициент a"
+factor_a = gets.to_f
+puts "Введите коофициент b"
+factor_b = gets.to_f
+puts "Введите коофициент c"
+factor_c = gets.to_f
+discr = ((factor_b**2) - (4 * factor_a * factor_c))
+if discr < 0
+  puts "Дискриминант = #{discr} Корней нет."
+elsif discr > 0
+  square_root = Math.sqrt(discr)
+  x1 = (-factor_b + square_root) / (2.0 * factor_a)
+  x2 = (-factor_b - square_root) / (2.0 * factor_a)
+  puts "Дискриминант = #{discr} Корень1 = #{x1} Корень2 = #{x2}"
+else #discr = 0
+  square_root = Math.sqrt(discr)
+  x1 = (-factor_b + square_root) / (2.0 * factor_a)
+  puts "Дискриминант = #{discr} Корень = #{x1}"
+
+end
--- lesson1/weight.rb
+++ lesson1/weight.rb
@@ -4,10 +4,10 @@ puts "#{name}, привет! Какой твой рост?"
 height = gets.to_i
 puts "#{name}, привет! Какой твой вес?"
 weight = gets.to_i
-ideal = height-110
-if weight <= ideal
+ideal_weight = height - 110
+if weight <= ideal_weight
   puts "Ваш вес уже оптимальный"
 else
-  puts "Ваш идеальный вес: #{height-110}"
+  puts "Ваш идеальный вес: #{height - 110}"
 end
 
